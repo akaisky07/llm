6ae195814a872cfbbb1b27df3bf8841a101e8ede
akaisky07 <amankumartandan001@gmail.com>
2024-01-27 11:47:28 +0530

    templates

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-# import gemini_client
-# api_key = "AIzaSyAiB2nQe1lfdip_h0Ap2wphPGzM-_bRJBY"
-# client = gemini_client.Client(api_key=api_key)
-
-# def generate_summary_with_gemini(function_code, function_name, metadata=None):
-#     prompt = f"Summarize the following function in a concise and informative way:\n\n{function_code}\n\nFunction name: {function_name}\n\n{metadata}"
-#     response = client.generate_text(prompt=prompt)
-#     return response.text
-
+from flask import Flask, render_template, request
 import os
 import google.generativeai as genai
 
-os.environ['GOOGLE_API_KEY'] = "AIzaSyAiB2nQe1lfdip_h0Ap2wphPGzM-_bRJBY"
-genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+app= Flask(__name__)
 
-from IPython.display import Markdown
+@app.route('/')
+def home():
+    return render_template('index.html',prediction=None)
 
-input="def add_numbers(*args):sum = 0for number in args:sum += numberreturn sum"
+@app.route('/predict', methods=['POST'])
+def predict():
+    input=request.form['input']
 
-model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content("Summarize the following function in a concise and informative way"+input)
+    os.environ['GOOGLE_API_KEY'] = "AIzaSyAiB2nQe1lfdip_h0Ap2wphPGzM-_bRJBY"
+    genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+    from IPython.display import Markdown
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content("Summarize the following function in a concise and informative way"+input)
 
-Markdown(response.text)
+    return render_template('index.html', prediction = response.text)
 
-print(response.text)
+if __name__=='__main__':
+    app.run(debug=True)
